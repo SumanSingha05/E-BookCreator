5c857f05369be5a3a84189077e80ba6ac96b9083
SumanSingha05 <sumansingha8654@gmail.com>
2025-10-19 11:43:03 +0530

    Protected routes added.

--- frontend/eBookCreator/src/App.jsx
+++ frontend/eBookCreator/src/App.jsx
@@ -3,7 +3,11 @@ import { Routes, Route } from 'react-router-dom';
 import ProtectedRoute from './components/auth/ProtectedRoute';
 import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
-import SignupPage from "./pages/SignupPage"
+import SignupPage from "./pages/SignupPage";
+import DashboardPage from "./pages/DashboardPage";
+import ViewBookPage from "./pages/ViewBookPage";
+import ProfilePage from "./pages/ProfilePage";
+import EditorPage from "./pages/EditorPage"
 
 
 const App = () => {
@@ -14,9 +18,27 @@ const App = () => {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+
+        {/* ProtectedRoute */}
+        <Route
+          path="/dashboard"
+          element={<ProtectedRoute><DashboardPage /></ProtectedRoute>}
+        />
+        <Route
+          path="/editor/:bookId"
+          element={<ProtectedRoute><EditorPage /></ProtectedRoute>}
+        />
+        <Route
+          path="/view-book/:bookId"
+          element={<ProtectedRoute><ViewBookPage /></ProtectedRoute>}
+        />
+        <Route
+          path="/profile"
+          element={<ProtectedRoute><ProfilePage /></ProtectedRoute>}
+        />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
